19cb4a7cb77e0d54346fac3196540e2e512b2602
Aayush Sharma <aayushramansharma@gmail.com>
2025-07-26 14:54:22 +0200

    mongo db initialization

---
+++ app/metadata_db.py
@@ -0,0 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+client = AsyncIOMotorClient("mongo://mongodb:27017")
+db = client.products
+
+async def get_product_metadata(product_id):
+    result = db.catalog.find_one({"product_id": product_id}, {"_id": 0})
+    return await result
