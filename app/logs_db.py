b73990334be0fafc63658769201676c0b2cbadb1
Aayush Sharma <aayushramansharma@gmail.com>
2025-07-26 15:00:00 +0200

    logs db

---
+++ app/logs_db.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import datetime
+import base64
+
+client = AsyncIOMotorClient("mongo://mongodb:27017")
+db = client.logs
+
+async def log_query(image_bytes, top_match_id, error=None):
+    log_entry = {"timestamp": datetime.datetime.now(),
+                 "input": base64.b64encode(image_bytes).decode('utf-8'),
+                 "top_match_id": top_match_id,
+                 "error": error}
+
+    await db.queries.insert_one(log_entry)
