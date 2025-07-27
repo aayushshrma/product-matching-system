562fe97f160eb98ef6e702c4d50617e9e9b84517
Aayush Sharma <aayushramansharma@gmail.com>
2025-07-27 12:01:47 +0200

    model load, export for quantization

---
+++ quantize_model.py
@@ -0,0 +1,29 @@
+import torch
+from transformers import CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32",
+                                  resume_download=True)
+model.eval()
+print("Model Downloaded!")
+
+dummy_image = torch.randn(1, 3, 224, 224)
+torch.onnx.export(model.vision_model,
+                  dummy_image,
+                  "clip_visual.onnx",
+                  input_names=["input_image"],
+                  output_names=["embedding"],
+                  dynamic_axes={"input_image": {0: "batch"}},
+                  opset_version=14)
+
+dummy_ids = torch.randint(0, 10000, (1,77))
+dummy_mask = torch.ones((1,77), dtype=torch.int64)
+
+torch.onnx.export(model.text_model,
+                  (dummy_ids, dummy_mask),
+                  "clip_text.onnx",
+                  input_names=["input_ids", "attention_mask"],
+                  output_names=["embedding"],
+                  dynamic_axes={"input_ids": {0: "batch"}, "attention_mask": {0: "batch"}},
+                  opset_version=14)
+
+print("Export Complete!")
