c9386661f946f32a1771d15d5eb75f33604bc9ab
Aayush Sharma <aayushramansharma@gmail.com>
2025-08-04 10:14:16 +0200

    new data

--- app/populate_db.py
+++ app/populate_db.py
@@ -1,34 +1,38 @@
-import base64
-import json
-from PIL import Image
-import numpy as np
 from metadata_db import db
 from vector_db import add_embedding_to_index
 from inference import process_image
-import asyncio
+from inference import process_text
+
 
 products = [{"product_id": "001",
              "name": "Brown Boots",
              "price": 3999,
              "category": "Footwear",
-             "image_path": "sample/boots.jpg"},
+             "image_path": "catalog/boots.jpg"},
             { "product_id": "002",
               "name": "Blue Hoodie",
               "price": 1499,
               "category": "Apparel",
-              "image_path": "sample/hoodie.jpg"},
+              "image_path": "catalog/hoodie.jpg"},
             {"product_id": "003",
              "name": "Tablet",
              "price": 19999,
              "category": "Electronics",
-             "image_path": "sample/tablet.jpg"}]
+             "image_path": "catalog/tablet.jpg"},
+            {"product_id": "004",
+             "name": "Table",
+             "price": 14999,
+             "category": "Furniture",
+             "image_path": "catalog/table.jpg"}]
 
 async def populate():
     for product in products:
         with open(product['image_path'], 'rb') as f:
             image_bytes = f.read()
-            emb = await process_image(image_bytes)
-            await add_embedding_to_index(product['product_id'], emb)
+        image_emb = await process_image(image_bytes=image_bytes)
+        text_emb = await process_text(input_text=product["name"])
+        await add_embedding_to_index(product_id=product['product_id'], image_emb=image_emb,
+                                     text_emb=text_emb)
 
         db.catalog.insert_one({"product_id": product['product_id'],
                                "name": product['name'],
@@ -36,6 +40,3 @@ async def populate():
                                "category": product['category'],
                                "image_url": product['image_path']})
 
-
-if __name__ == "__main__":
-    asyncio.run(populate())
