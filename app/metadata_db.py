b7bd3174330ff7cfaabc308e1611372f33db65f6
Aayush Sharma <aayushramansharma@gmail.com>
2025-08-04 10:05:53 +0200

    clear collection function

--- app/metadata_db.py
+++ app/metadata_db.py
@@ -1,7 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+
 client = AsyncIOMotorClient("mongodb://localhost:27017")
+
 db = client.products
 
 async def get_product_metadata(product_id):
     result = db.catalog.find_one({"product_id": product_id}, {"_id": 0})
     return await result
+
+async def clear_catalog():
+    await db.catalog.delete_many({})
