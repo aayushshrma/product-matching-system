9b1c6944f90c85afe4baa481f58edacf1a342038
Aayush Sharma <aayushramansharma@gmail.com>
2025-08-04 10:04:03 +0200

    consideration of text embeddings

--- app/vector_db.py
+++ app/vector_db.py
@@ -1,16 +1,21 @@
 import faiss
 import numpy as np
 
-index = faiss.IndexFlatL2(512)
+index = faiss.IndexFlatL2(1024)  # argmin||x - xi||
 product_ids = []
 
-
-async def add_embedding_to_index(product_id, embedding):
-    index.add(np.array([embedding]).astype('float32'))
+async def add_embedding_to_index(product_id, image_emb, text_emb):
+    combined_emb = np.concatenate([image_emb, text_emb], axis=-1)
+    index.add(np.array([combined_emb]).astype('float32'))
     product_ids.append(product_id)
 
-
-async def search_nearest_embeddings(query_embedding, top_k=5):
-    D, I = index.search(np.array([query_embedding]).astype('float32'), top_k)
-    result = [{"product_id": product_ids[i], "distance": float(D[0][j])} for j,i in enumerate(I[0])]
+async def search_nearest_embeddings(image_emb, text_emb, top_k=5):
+    combined_emb = np.concatenate([image_emb, text_emb], axis=-1)
+    dist, idx = index.search(np.array([combined_emb]).astype('float32'), top_k)
+    result = [{"product_id": product_ids[i], "distance": float(dist[0][j])} for j,i in enumerate(idx[0])]
     return result
+
+def reset_faiss_index():
+    global index, product_ids
+    index.reset()
+    product_ids = []
